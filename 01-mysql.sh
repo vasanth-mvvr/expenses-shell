616727948d0fdd43e8ce9ebb860bdec7c9c8f841
vasanth-mvvr <vasanthreddy@example.com>
2025-07-21 00:15:58 +0530

    updated

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -43,14 +43,14 @@ VALIDATE $? "Starting service of mysql-server"
 #Here the disadvantage is that once we set up the password it cannot be repeated and we should make sure that it is idempotent in nature in the shell script .
 #Idempotent means how many ever times you run it should of be the same.
 
-# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-# VALIDATE $? "Setting up the root password"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+VALIDATE $? "Setting up the root password"
 
-mysql -h 172.31.85.69 -uroot -p${my_sql_password} -e 'SHOW DATABASES;' &>>$LOGFILE
-if [ $? -ne 0 ]
-then 
-    mysql_secure_installation --set-root-pass ${my_sql_password}
-    VALIDATE $? "Password setup " &>>$LOGFILE
-else
-    echo -e "Password is already set .. $Y Skipping $N"
-fi
+# mysql -h db.vasanthreddy.space -uroot -p${my_sql_password} -e 'SHOW DATABASES;' &>>$LOGFILE
+# if [ $? -ne 0 ]
+# then 
+#     mysql_secure_installation --set-root-pass ${my_sql_password}
+#     VALIDATE $? "Password setup " &>>$LOGFILE
+# else
+#     echo -e "Password is already set .. $Y Skipping $N"
+# fi

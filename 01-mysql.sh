89b367f238ca0fa97a6736f9c59a1b5eb6821ae5
vasanth-mvvr <vasanthreddy@example.com>
2025-07-20 09:30:05 +0530

    updated

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERID=$(id -u)
-TIMESTAMP=$(+date +%F-%H-%M-%S)
+TIMESTAMP=$(date +%F-%I:%M:%S-%p)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 

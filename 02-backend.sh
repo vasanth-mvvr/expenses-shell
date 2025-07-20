fb7bc40295a26eaf942a1b197cf109d71791cb39
vasanth-mvvr <vasanthreddy@example.com>
2025-07-21 00:36:07 +0530

    updated

--- 02-backend.sh
+++ 02-backend.sh
@@ -55,6 +55,7 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 VALIDATE $? "Extracted backend code"
 
 cd /app
+rm -rf /app/* &>>$LOGFILE
 unzip /tmp/backend.zip &>>$LOGFILE
 VALIDATE $? "Extracted file successfully"
 

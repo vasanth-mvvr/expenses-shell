360ae50a85627c4eab6cd39a235aefd94ba1df60
vasanth-mvvr <vasanthreddy@example.com>
2025-07-20 19:09:48 +0530

    first commit

---
+++ 02-backend.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%I:%H:%M-%p)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "Please enter the password"
+read -s my_sql_password
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2  $R Failed $N"
+    else 
+        echo -e "$2 $G Success $N"
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "you are not super user"
+else 
+    echo "you are super user"
+fi
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disabled nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enabled nodejs version --20" 
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installed nodejs successfully"
+
+id expense &>>$LOGFILE
+if [ $id -ne 0 ]
+then 
+    useradd expense &>>$LOGFILE
+    VALIDATE $? "user added successfully"
+else
+    echo "User is already created $Y Skipping $N"
+
+mkdir -p /app  #Here -p is used for validating if the directory is present it ignores or else it creates a new directory
+VALIDATE $? "directory created successfully"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+VALIDATE $? "Extracted backend code"
+
+cd /app
+unzip /tmp/backend.zip
+VALIDATE $? "Extracted file successfully"
+
+npm install 
+VALIDATE $? "Installed dependencies"
+
+cp /home/ec2-user/expenses-shell/backend.service /etc/systemd/system/backend.service
+VALIDATE $? "backend.service intiated successfully"
+
+systemctl daemon-reload
+VALIDATE $? "deamon-reload backend"
+
+systemctl start backend
+VALIDATE $? "Started backend"
+
+systemctl enable backend
+VALIDATE $? "Enabled backend"
+
+dnf install mysql -y
+VALIDATE $? "Installed mysql"
+
+mysql -h db.vasanthreddy.space -uroot -p${my_sql_password} < /app/schema/backend.sql
+VALIDATE $? "Schema loaded"
+
+systemctl restart mysql
+VALIDATE $? "Restarted mysql"
+
+

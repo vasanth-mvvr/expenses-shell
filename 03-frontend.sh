6118f0a54c4b2421ac4076417ab77d7d5ea92ec1
vasanth-mvvr <vasanthreddy@example.com>
2025-07-21 01:03:07 +0530

    updated

--- 03-frontend.sh
+++ 03-frontend.sh
@@ -14,6 +14,7 @@ VALIDATE(){
     if [ $1 -ne 0 ]
     then 
         echo -e "$2  $R Failed $N"
+        exit 1
     else 
         echo -e "$2 $G Success $N"
 }
@@ -21,6 +22,7 @@ VALIDATE(){
 if [ $USERID -ne 0 ]
 then 
     echo "you are not super user"
+    exit 1
 else 
     echo "you are super user"
 fi
